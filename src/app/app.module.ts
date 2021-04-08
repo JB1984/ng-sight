5ac5c39a3b58c12f7aa0724b4534210c27e5231c
Jason Benner <jbenner1984@gmail.com>
2021-04-08 15:04:52 -0400

    Added bar chart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,10 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 import { SectionSalesComponent } from './section-sales/section-sales.component';
 import { SectionOrdersComponent } from './section-orders/section-orders.component';
 import { SectionHealthComponent } from './section-health/section-health.component';
+import { BarChartComponent } from './bar-chart/bar-chart.component';
+import { LineChartComponent } from './line-chart/line-chart.component';
+import { PieChartComponent } from './pie-chart/pie-chart.component';
+import { ChartsModule } from 'ng2-charts';
 
 
 @NgModule({
@@ -17,11 +21,15 @@ import { SectionHealthComponent } from './section-health/section-health.componen
     SidebarComponent,
     SectionSalesComponent,
     SectionOrdersComponent,
-    SectionHealthComponent
+    SectionHealthComponent,
+    BarChartComponent,
+    LineChartComponent,
+    PieChartComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes), 
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
