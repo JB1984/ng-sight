b02833e279d40cef27e83418a590ef44f157e8bf
Jason Benner <jbenner1984@gmail.com>
2021-04-11 19:59:27 -0400

    Added Latest Orders Table

--- src/app/section-orders/section-orders.component.ts
+++ src/app/section-orders/section-orders.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Order } from "../shared/order";
 
 @Component({
   selector: 'app-section-orders',
@@ -8,6 +9,15 @@ import { Component, OnInit } from '@angular/core';
 export class SectionOrdersComponent implements OnInit {
 
   constructor() { }
+
+  orders: Order[] = [
+    {id: 1, customer: {id:1, name:"Main St Bakery", state:'CO', email: 'mstbakery@gmail.com'}, total:230, placed: new Date(2020,1,1), fulfilled: new Date(2020,2,1)},
+    {id: 1, customer: {id:1, name:"Main St Bakery", state:'CO', email: 'mstbakery@gmail.com'}, total:230, placed: new Date(2020,1,1), fulfilled: new Date(2020,2,1)},
+    {id: 1, customer: {id:1, name:"Main St Bakery", state:'CO', email: 'mstbakery@gmail.com'}, total:230, placed: new Date(2020,1,1), fulfilled: new Date(2020,2,1)},
+    {id: 1, customer: {id:1, name:"Main St Bakery", state:'CO', email: 'mstbakery@gmail.com'}, total:230, placed: new Date(2020,1,1), fulfilled: new Date(2020,2,1)},
+    {id: 1, customer: {id:1, name:"Main St Bakery", state:'CO', email: 'mstbakery@gmail.com'}, total:230, placed: new Date(2020,1,1), fulfilled: new Date(2020,2,1)}
+  ];
+
 
   ngOnInit(): void {
   }
