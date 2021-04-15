80c40c525f951abb1eb366399d6680d35c03c540
Jason Benner <jbenner1984@gmail.com>
2021-04-14 20:37:57 -0400

    Added System Health Section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { BarChartComponent } from './bar-chart/bar-chart.component';
 import { LineChartComponent } from './line-chart/line-chart.component';
 import { PieChartComponent } from './pie-chart/pie-chart.component';
 import { ChartsModule } from 'ng2-charts';
+import { ServerComponent } from './server/server.component';
 
 
 @NgModule({
@@ -24,7 +25,8 @@ import { ChartsModule } from 'ng2-charts';
     SectionHealthComponent,
     BarChartComponent,
     LineChartComponent,
-    PieChartComponent
+    PieChartComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule,
--- src/app/section-orders/section-orders.component.ts
+++ src/app/section-orders/section-orders.component.ts
@@ -12,10 +12,10 @@ export class SectionOrdersComponent implements OnInit {
 
   orders: Order[] = [
     {id: 1, customer: {id:1, name:"Main St Bakery", state:'CO', email: 'mstbakery@gmail.com'}, total:230, placed: new Date(2020,1,1), fulfilled: new Date(2020,2,1)},
-    {id: 1, customer: {id:1, name:"Main St Bakery", state:'CO', email: 'mstbakery@gmail.com'}, total:230, placed: new Date(2020,1,1), fulfilled: new Date(2020,2,1)},
-    {id: 1, customer: {id:1, name:"Main St Bakery", state:'CO', email: 'mstbakery@gmail.com'}, total:230, placed: new Date(2020,1,1), fulfilled: new Date(2020,2,1)},
-    {id: 1, customer: {id:1, name:"Main St Bakery", state:'CO', email: 'mstbakery@gmail.com'}, total:230, placed: new Date(2020,1,1), fulfilled: new Date(2020,2,1)},
-    {id: 1, customer: {id:1, name:"Main St Bakery", state:'CO', email: 'mstbakery@gmail.com'}, total:230, placed: new Date(2020,1,1), fulfilled: new Date(2020,2,1)}
+    {id: 2, customer: {id:1, name:"Main St Bakery", state:'CO', email: 'mstbakery@gmail.com'}, total:230, placed: new Date(2020,1,1), fulfilled: new Date(2020,2,1)},
+    {id: 3, customer: {id:1, name:"Main St Bakery", state:'CO', email: 'mstbakery@gmail.com'}, total:230, placed: new Date(2020,1,1), fulfilled: new Date(2020,2,1)},
+    {id: 4, customer: {id:1, name:"Main St Bakery", state:'CO', email: 'mstbakery@gmail.com'}, total:230, placed: new Date(2020,1,1), fulfilled: new Date(2020,2,1)},
+    {id: 5, customer: {id:1, name:"Main St Bakery", state:'CO', email: 'mstbakery@gmail.com'}, total:230, placed: new Date(2020,1,1), fulfilled: new Date(2020,2,1)}
   ];
 
 
